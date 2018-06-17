6577c8298451754ee287cfea6f7cd7eea7838509
Serhiy Yakovlev <ysa.sergey@gmail.com>
2018-06-17 19:50:03 +0300

    Example answers for https://www.testdome.com/d/java-interview-questions/4

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,34 @@
+class Node {
+    public int value;
+    public Node left, right;
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinarySearchTree {
+    public static boolean contains(Node root, int value) {
+        Node current = root;
+        while(current!=null){
+            if(current.value==value){
+                return true;
+            }else if(current.value>value){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1, null, null);
+        Node n3 = new Node(3, null, null);
+        Node n2 = new Node(2, n1, n3);
+
+        System.out.println(contains(n2, 3));
+    }
+}
---
+++ src/TwoSum.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TwoSum {
+
+    public static int[] findTwoSum(int[] list, int sum) {
+        Map<Integer, Integer> numPositions = new HashMap<>();
+        if (list == null || list.length <= 1)
+            return null;
+
+        for (int i = 0; i < list.length; i++) {
+
+            int toRetrieve = sum - list[i];
+
+            if (numPositions.containsKey(toRetrieve)) {
+                if (toRetrieve + list[i] == sum)
+                    return new int[]{numPositions.get(toRetrieve), i};
+            }
+            numPositions.put(list[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] indices = findTwoSum(new int[] { 3, 1, 5, 7, 5, 9 }, 10);
+
+
+        if(indices != null) {
+            System.out.println(indices[0] + " " + indices[1]);
+        }
+    }
+}
